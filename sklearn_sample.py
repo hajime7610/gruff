821586f257e8d91b2a85de9bb4c00657e598135a
hajime7610 <94503610+hajime7610@users.noreply.github.com>
2021-12-01 12:43:41 +0900

    Update sklearn_sample.py

--- sklearn_sample.py
+++ sklearn_sample.py
@@ -34,36 +34,47 @@ sc.fit(df)
 # 実際にデータを変換
 z = sc.transform(df)
 
-print(z)
-print(z.shape)
+
 
 #モデル生成、因子得点の算出
 # sklearnのFactorAnalysis(因子分析)クラスをインポート
 from sklearn.decomposition import FactorAnalysis as FA
 
 # 因子数を指定
-n_components=3
+n_components=4
 
 # 因子分析の実行
 fa = FA(n_components, max_iter=5000) # モデルを定義
 fitted = fa.fit_transform(z) # fitとtransformを一括処理
 
-print(fitted)
-print(fitted.shape)
+#print(fitted)
+#print(fitted.shape)
 
 #因子負荷量行列
 fa.components_.T
+print(fa.components_.T.shape)
 
 # 変数Factor_loading_matrixに格納
 Factor_loading_matrix = fa.components_.T
 
 # データフレームに変換
 df_2 = pd.DataFrame(Factor_loading_matrix, 
-             columns=["第1因子", "第2因子", "第3因子"], 
-             index=[df.columns])
+             columns=["第1因子", "第2因子", "第3因子","第4因子"], 
+             index=[df.columns])# 
 
+df_2.style.set_caption("factor number:{}".format(str(n_components)))
 #csvファイルに出力
-df_2.to_csv("seiseki_factor.csv")
+df_2.to_csv("seiseki_factor_{}.csv".format(str(n_components)))
 
+num = df.shape[1]
+fig = plt.figure(figsize=(15,15))
+for i in range(num):
+    ax = fig.add_subplot(3,3,i+1)
+    ax.scatter(fitted[:, 0], fitted[:, 1], alpha=0.8, c=list(df.iloc[:, i]))
+    ax.grid()
+    plt.xlabel("第1因子")
+    plt.ylabel("第2因子")
+    plt.title(df.columns.values[i])
 
+plt.savefig("factor_scatter.png")
 
